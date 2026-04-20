33f73887af57fbf6d5b939e8c842885f124c4d9a
Daniel Fabelo <daniel-fabelo-santana@outlook.es>
2026-04-20 15:45:43 +0000

    First Save

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,20 +7,12 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+            <div className="trafficTop"></div>
+			<div className="container"></div>
+			<div className="red light selected"></div>
+			<div className="yellow light"></div>
+			<div className="green light"></div>
+			<button>cambio de color</button>
 		</div>
 	);
 };
